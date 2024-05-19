3637b6089380ea5fe72549ffe61a74ddb9fe722f
valha <49664001+valha@users.noreply.github.com>
2024-05-18 22:52:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# laboratorio_3
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/laboratorio_3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.laboratorio_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
