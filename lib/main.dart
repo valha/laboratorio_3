425927cdfe68d319baafe03c92d2144f400b6870
valha <49664001+valha@users.noreply.github.com>
2024-05-19 00:03:41 -0400

    finished lab 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,98 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int value = 0;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.black,
+            title: const Text(
+              "Math",
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 80),
+            child: FloatingActionButton(
+              onPressed: () {
+                resetValue();
+              },
+              backgroundColor: Colors.black,
+              child: Icon(
+                Icons.account_balance,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          persistentFooterButtons: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      minusOne();
+                    },
+                    child: Icon(Icons.remove)),
+                ElevatedButton(
+                    onPressed: () {
+                      plusOne();
+                    },
+                    child: Icon(Icons.add)),
+              ],
+            ),
+          ],
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                value.toString(),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )
+            ],
+          ))),
     );
+  }
+
+  void plusOne() {
+    setState(() {
+      value++;
+    });
+  }
+
+  void minusOne() {
+    setState(() {
+      value--;
+    });
+  }
+
+  void resetValue() {
+    setState(() {
+      value = 0;
+    });
   }
 }
